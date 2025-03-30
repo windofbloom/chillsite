27e654adf52d1ddc9c9f8019e94680700a8bcc8d
bloom <bloomillus@gmail.com>
2025-03-30 18:04:28 -0500

    Editing js to animate responsive menu entrance and exit, animated about section entrance

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -5,7 +5,32 @@ function showSidebar() {
     sidebar.style.display = 'flex';
 }
 
-function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'none';
+function sidebarEntrace() {
+    const element = document.querySelector('.sidebar');
+    element.classList.add('animate__animated', 'animate__flipInY');
+}
+
+//function hideSidebar() {
+//    const sidebar = document.querySelector('.sidebar');
+//    sidebar.style.display = 'none';
+//}
+
+function sidebarExit() {
+    const element = document.querySelector('.sidebar');
+    element.classList.add('animate__animated', 'animate__flipOutY');
+
+    element.addEventListener('animationend', () => {
+        element.style.display = 'none';
+        element.classList.remove('animate__animated', 'animate__flipOutY'); 
+    }, { once: true }); 
+}
+
+
+function animatedSidebarEntrace() {
+    showSidebar();
+    sidebarEntrace();
+}
+
+function animatedSidebarExit() {
+    sidebarExit();
 }
